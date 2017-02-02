188154a91aea500e498bb3d173733ef6e44482bd
Alice Easter <alice.easter@mymail.champlain.edu>
2017-02-01 23:52:40 -0500

    initialization

---
+++ Control/Controller.swift
@@ -0,0 +1,9 @@
+//
+//  Controller.swift
+//  Control
+//
+//  Created by Alice Easter on 1/19/17.
+//  Copyright © 2017 Alice Easter. All rights reserved.
+//
+
+import Foundation
---
+++ Control/Reachability.swift
@@ -0,0 +1,9 @@
+//
+//  Reachability.swift
+//  Control
+//
+//  Created by Alice Easter on 1/28/17.
+//  Copyright © 2017 Alice Easter. All rights reserved.
+//
+
+import Foundation
